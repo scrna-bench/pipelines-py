e385a26b4d7c9435335cb3d9232c65ff5035c166
Tom Kitak <tomaz.kitak@gmail.com>
2026-01-09 11:20:02 +0100

    omnibenchmark adaptation

--- scanpy_sc_mix.py
+++ scanpy_sc_mix.py
@@ -3,9 +3,12 @@
 # Modified by: Tom Kitak (Omnibenchmark compatibility)
 # License: MIT (see repository LICENSE)
 
+import argparse
+import json
+import os
 
 import numpy as np
-import pandas as pd 
+import pandas as pd
 import scanpy as sc
 import time
 import anndata
@@ -13,21 +16,28 @@ from sklearn.metrics import adjusted_rand_score
 from sklearn.metrics import silhouette_score
 
 
+# Parse command line arguments
+parser = argparse.ArgumentParser(description="Scanpy single-cell analysis pipeline")
+parser.add_argument("--output_dir", "-o", type=str, required=True, help="Output directory")
+parser.add_argument("--name", "-n", type=str, required=True, help="Dataset name")
+parser.add_argument("--data.h5ad", dest="data_h5ad", type=str, required=True, help="Input h5ad file")
+parser.add_argument("--methods", type=str, default="scanpy", help="Method to run")
+args, _ = parser.parse_known_args()
 
+# Create output directory
+os.makedirs(args.output_dir, exist_ok=True)
 
 sc.settings.verbosity = 3
 
-# save time usage #### 
+# save time usage ####
 time_sc = pd.DataFrame(index=["find_mit_gene", "filter", "normalization", "hvg",
                            "scaling", "PCA", "t-sne", "umap", "louvain", "leiden"],
                     columns=["time_sec"])
 
 
 # data ####
-
-#adata = sc.read_h5ad("/mnt/spca/pipeline_sc/sc_mixolgy_10x_5cl.h5ad")
-adata = sc.read_h5ad("sc_mixolgy_10x_5cl.h5ad")
-adata.var_names_make_unique()  
+adata = sc.read_h5ad(args.data_h5ad)
+adata.var_names_make_unique()
 adata
 
 
@@ -36,12 +46,12 @@ sc.pl.highest_expr_genes(adata, n_top=20, )
 # find mitocondrial genes ####
 start_time = time.time()
 
-adata.var['mt'] = adata.var_names.str.startswith('MT-') 
+adata.var['mt'] = adata.var_names.str.startswith('MT-')
 sc.pp.calculate_qc_metrics(adata, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
 
 sc.pl.violin(adata, ['n_genes_by_counts', 'total_counts', 'pct_counts_mt'],
              jitter=0.4, multi_panel=True)
-             
+
 end_time = time.time()
 time_elapsed = end_time - start_time
 print("Time Elapsed:", time_elapsed)
@@ -80,7 +90,7 @@ start_time = time.time()
 
 sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5,
 n_top_genes = 1000)
-sc.pl.highly_variable_genes(adata) 
+sc.pl.highly_variable_genes(adata)
 
 adata.raw = adata
 adata = adata[:, adata.var.highly_variable]
@@ -92,7 +102,7 @@ time_sc.iloc[3, 0] = time_elapsed
 
 
 df = pd.DataFrame(adata.var.highly_variable, columns=['hvg'])
-df.to_excel('sc_mix_scanpy_hvg.xlsx', index=False)
+df.to_excel(os.path.join(args.output_dir, f'{args.name}_hvg.xlsx'), index=False)
 
 
 # Scaling the data ####
@@ -114,7 +124,7 @@ start_time = time.time()
 sc.tl.pca(adata, svd_solver='arpack')
 sc.pl.pca(adata, color='CST3')
 sc.pl.pca_variance_ratio(adata, log=True)
- 
+
 adata
 
 end_time = time.time()
@@ -201,4 +211,30 @@ print("Average Silhouette Score:", silhouette_avg)
 
 time_sc
 
-adata.write("sc_mix_scanpy.h5ad")
+# Save outputs for omnibenchmark ####
+
+# Save timings as JSON
+timings_dict = time_sc['time_sec'].to_dict()
+with open(os.path.join(args.output_dir, f'{args.name}.timings.json'), 'w') as f:
+    json.dump(timings_dict, f, indent=2)
+
+# Save cluster assignments as TSV
+cluster_df = pd.DataFrame({
+    'cell_id': adata.obs_names,
+    'louvain': adata.obs['louvain'].values,
+    'leiden': adata.obs['leiden'].values
+})
+if 'cell_line' in adata.obs.columns:
+    cluster_df['cell_line'] = adata.obs['cell_line'].values
+cluster_df.to_csv(os.path.join(args.output_dir, f'{args.name}.cluster.tsv'), sep='\t', index=False)
+
+# Save PCA coordinates as TSV
+pca_df = pd.DataFrame(
+    adata.obsm['X_pca'],
+    index=adata.obs_names,
+    columns=[f'PC{i+1}' for i in range(adata.obsm['X_pca'].shape[1])]
+)
+pca_df.to_csv(os.path.join(args.output_dir, f'{args.name}.pca.tsv'), sep='\t', index=True)
+
+# Also save the full h5ad file
+adata.write(os.path.join(args.output_dir, f'{args.name}.h5ad'))
